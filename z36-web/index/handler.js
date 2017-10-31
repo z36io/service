ae4bec0076bfce8597b8a1dab2d32017c61edc7b
z36io <mail@z36.io>
2017-10-31 15:40:41 +0000

    split for micro services

---
+++ z36-web/index/handler.js
@@ -0,0 +1,15 @@
+var lib = require('../../lib/template.js');
+
+module.exports.hello = (event, context, callback) => {
+  var response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: lib.message,
+      event: event,
+      context: context
+    }),
+  };
+
+  callback(null, response);
+
+};
