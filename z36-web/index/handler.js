d490603a27e8c38e41173ec5734bacbec8ba51fe
z36io <mail@z36.io>
2017-11-21 12:05:58 +0000

    landing page

--- z36-web/index/handler.js
+++ z36-web/index/handler.js
@@ -3,8 +3,8 @@ var pug = require('pug');
 module.exports.hello = (event, context, callback) => {
   var i18n = require('./i18n.json').en;
   var html = pug.renderFile(__dirname + '/template.pug', {
-    title: 'z36.io',
-    slogan: i18n.slogan
+    title: i18n.title,
+    i18n: i18n
   });
   var response = {
     statusCode: 200,
